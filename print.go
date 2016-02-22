0a24d79e6056bbec9257e09a38cd18d7706c785a
J. Brandt Buckley <brandt@runlevel1.com>
2016-02-21 21:06:55 -0700

    Wrap and indent long DNs

--- print.go
+++ print.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/brandt/certfetch/isatty"
 )
@@ -391,10 +392,12 @@ func printChainPaths(chains [][]*x509.Certificate) {
 			} else {
 				cert_type = "INTR"
 			}
-			dn := NamesToDN(cert.Subject.Names)
+			dn := wrapString(NamesToDN(cert.Subject.Names), 58, 13)
 			printStderr("  %d.  %-6s %s\n", n, cert_type, dn)
 		}
 	}
+
+	printNewline()
 }
 
 func NamesToDN(names []pkix.AttributeTypeAndValue) string {
@@ -435,4 +438,67 @@ func getTagForOid(oid asn1.ObjectIdentifier) string {
 	}
 
 	return fmt.Sprint(oid)
+}
+
+func wrapString(s string, lim uint, indent int) string {
+	// Initialize a buffer with a slightly larger size to account for breaks
+	init := make([]byte, 0, len(s))
+	buf := bytes.NewBuffer(init)
+
+	var current uint
+	var wordBuf, spaceBuf bytes.Buffer
+
+	for _, char := range s {
+		if char == '\n' {
+			if wordBuf.Len() == 0 {
+				if current+uint(spaceBuf.Len()) > lim {
+					current = 0
+				} else {
+					current += uint(spaceBuf.Len())
+					spaceBuf.WriteTo(buf)
+				}
+				spaceBuf.Reset()
+			} else {
+				current += uint(spaceBuf.Len() + wordBuf.Len())
+				spaceBuf.WriteTo(buf)
+				spaceBuf.Reset()
+				wordBuf.WriteTo(buf)
+				wordBuf.Reset()
+			}
+			buf.WriteRune(char)
+			current = 0
+		} else if unicode.IsSpace(char) {
+			if spaceBuf.Len() == 0 || wordBuf.Len() > 0 {
+				current += uint(spaceBuf.Len() + wordBuf.Len())
+				spaceBuf.WriteTo(buf)
+				spaceBuf.Reset()
+				wordBuf.WriteTo(buf)
+				wordBuf.Reset()
+			}
+
+			spaceBuf.WriteRune(char)
+		} else {
+
+			wordBuf.WriteRune(char)
+
+			if current+uint(spaceBuf.Len()+wordBuf.Len()) > lim && uint(wordBuf.Len()) < lim {
+				buf.WriteRune('\n')
+				current = 0
+				spaceBuf.Reset()
+			}
+		}
+	}
+
+	if wordBuf.Len() == 0 {
+		if current+uint(spaceBuf.Len()) <= lim {
+			spaceBuf.WriteTo(buf)
+		}
+	} else {
+		spaceBuf.WriteTo(buf)
+		wordBuf.WriteTo(buf)
+	}
+
+	intentStr := "\n"
+	intentStr += strings.Repeat(" ", indent)
+	return strings.Replace(buf.String(), "\n", intentStr, -1)
 }
